e95325b36435eb119e5e323a0d7ed816c33761c6
Pi3raldoSanturro <santurro.pieraldo99@gmail.com>
2024-06-08 11:04:53 +0200

    Update spark_exec.sh

--- SABD-project1/Scripts/spark_exec.sh
+++ SABD-project1/Scripts/spark_exec.sh
@@ -7,11 +7,11 @@
 NUM_COR=3 
 
 # Copy the query files into the Spark container
-# docker cp ./Spark/Queries/Query1.py spark-master:/tmp/Query1.py
-# docker cp ./Spark/Queries/Query1_SQL.py spark-master:/tmp/Query1_SQL.py
-# docker cp ./Spark/Queries/Query2.py spark-master:/tmp/Query2.py
-# docker cp ./Spark/Queries/Query2SQL.py spark-master:/tmp/Query2SQL.py
-# docker cp ./Spark/Queries/Query3.py spark-master:/tmp/Query3.py
+docker cp ./Spark/Queries/Query1.py spark-master:/tmp/Query1.py
+docker cp ./Spark/Queries/Query1_SQL.py spark-master:/tmp/Query1_SQL.py
+docker cp ./Spark/Queries/Query2.py spark-master:/tmp/Query2.py
+docker cp ./Spark/Queries/Query2SQL.py spark-master:/tmp/Query2SQL.py
+docker cp ./Spark/Queries/Query3.py spark-master:/tmp/Query3.py
 docker cp ./Spark/Queries/Query3SQL.py spark-master:/tmp/Query3SQL.py
 
 
@@ -20,44 +20,44 @@ docker cp ./Spark/Queries/Query3SQL.py spark-master:/tmp/Query3SQL.py
 # Note: Adjust the resource configurations (--executor-memory and --total-executor-cores) as needed
 
 # # Execute Query1.py
-# docker exec -it spark-master /opt/bitnami/spark/bin/spark-submit \
-#     --master spark://spark-master:7077 \
-#     --conf spark.hadoop.fs.defaultFS=hdfs://master:54310 \
-#     --executor-memory 512m \
-#     --total-executor-cores $NUM_COR \
-#     /tmp/Query1.py
+docker exec -it spark-master /opt/bitnami/spark/bin/spark-submit \
+    --master spark://spark-master:7077 \
+    --conf spark.hadoop.fs.defaultFS=hdfs://master:54310 \
+    --executor-memory 512m \
+    --total-executor-cores $NUM_COR \
+    /tmp/Query1.py
 
 # # Execute Query1_SQL.py
-# docker exec -it spark-master /opt/bitnami/spark/bin/spark-submit \
-#     --master spark://spark-master:7077 \
-#     --conf spark.hadoop.fs.defaultFS=hdfs://master:54310 \
-#     --executor-memory 512m \
-#     --total-executor-cores $NUM_COR \
-#     /tmp/Query1_SQL.py
+docker exec -it spark-master /opt/bitnami/spark/bin/spark-submit \
+    --master spark://spark-master:7077 \
+    --conf spark.hadoop.fs.defaultFS=hdfs://master:54310 \
+    --executor-memory 512m \
+    --total-executor-cores $NUM_COR \
+    /tmp/Query1_SQL.py
 
 # # Execute Query2.py
-# docker exec -it spark-master /opt/bitnami/spark/bin/spark-submit \
-#     --master spark://spark-master:7077 \
-#     --conf spark.hadoop.fs.defaultFS=hdfs://master:54310 \
-#     --executor-memory 512m \
-#     --total-executor-cores $NUM_COR \
-#     /tmp/Query2.py
+docker exec -it spark-master /opt/bitnami/spark/bin/spark-submit \
+    --master spark://spark-master:7077 \
+    --conf spark.hadoop.fs.defaultFS=hdfs://master:54310 \
+    --executor-memory 512m \
+    --total-executor-cores $NUM_COR \
+    /tmp/Query2.py
 
 # # Execute Query2SQL.py
-# docker exec -it spark-master /opt/bitnami/spark/bin/spark-submit \
-#     --master spark://spark-master:7077 \
-#     --conf spark.hadoop.fs.defaultFS=hdfs://master:54310 \
-#     --executor-memory 512m \
-#     --total-executor-cores $NUM_COR \
-#     /tmp/Query2SQL.py
+docker exec -it spark-master /opt/bitnami/spark/bin/spark-submit \
+    --master spark://spark-master:7077 \
+    --conf spark.hadoop.fs.defaultFS=hdfs://master:54310 \
+    --executor-memory 512m \
+    --total-executor-cores $NUM_COR \
+    /tmp/Query2SQL.py
 
 # # Execute Query3.py
-# docker exec -it spark-master /opt/bitnami/spark/bin/spark-submit \
-#     --master spark://spark-master:7077 \
-#     --conf spark.hadoop.fs.defaultFS=hdfs://master:54310 \
-#     --executor-memory 512m \
-#     --total-executor-cores $NUM_COR \
-#     /tmp/Query3.py
+docker exec -it spark-master /opt/bitnami/spark/bin/spark-submit \
+    --master spark://spark-master:7077 \
+    --conf spark.hadoop.fs.defaultFS=hdfs://master:54310 \
+    --executor-memory 512m \
+    --total-executor-cores $NUM_COR \
+    /tmp/Query3.py
 
 # Execute Query3SQL.py
 docker exec -it spark-master /opt/bitnami/spark/bin/spark-submit \
